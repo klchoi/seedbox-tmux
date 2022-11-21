85e32e16fff061dc576e18ba3387d2fb5fe90c0e
klchoi <66675595+klchoi@users.noreply.github.com>
2022-11-22 01:56:00 +0700

    Update seedbox-tmux.fish

--- conf.d/seedbox-tmux.fish
+++ conf.d/seedbox-tmux.fish
@@ -1,4 +1,4 @@
-function _seedbox-tmux_install -e seedbox-tmux_install -e seedbox-tmux_update
+function _seedbox_tmux_install -e seedbox-tmux_install -e seedbox-tmux_update
   mkdir -p $HOME/.local/bin
   wget https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b-x86_64.AppImage -O $HOME/.local/bin/tmux
   chmod +x $HOME/.local/bin/tmux
